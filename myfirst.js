300cf9c3cc7db4a0ab0914f5d57ad28cf86f7ce9
balheru <valeriu.ioan.potra@gmail.com>
2018-06-21 08:54:20 -0400

    Added Some Comments

--- myfirst.js
+++ myfirst.js
@@ -3,16 +3,24 @@ const app = express()
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.post('/', function (req, res) {
+// simple post request
+app.post('/', function(req, res) {
   res.send('Got a POST request')
 })
 
-app.put('/user', function (req, res) {
+// simple put request
+app.put('/user', function(req, res) {
   res.send('Got a PUT request at /user')
 })
 
-app.delete('/user', function (req, res) {
+// simple delete request
+app.delete('/user', function(req, res) {
   res.send('Got a DELETE request at /user')
+})
+
+//simple get request with params
+app.get('/users/:userId', function(req, res) {
+  res.send(req.params)
 })
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
