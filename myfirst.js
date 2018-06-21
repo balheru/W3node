fdf78131faad966998d117e282438c916c72bffe
balheru <valeriu.ioan.potra@gmail.com>
2018-06-21 11:50:20 -0400

    SOB, finally figured out how to run through a json string

--- myfirst.js
+++ myfirst.js
@@ -1,7 +1,33 @@
 const express = require('express')
 const app = express()
+let request = require('request');
+var moment = require('moment');
+
 
 app.get('/', (req, res) => res.send('Hello World!'))
+
+
+
+let lat = 45.536883
+let lng = -73.674726
+// https://api.sunrise-sunset.org/json?lat=45.536883&lng=-73.674726
+let url = `https://api.sunrise-sunset.org/json?lat=${lat}&lng=${lng}`
+
+app.get('/sunrise',(req,res) => {
+
+  request(url, function (err, response, body) {
+  if(err){
+    console.log('error:', error);
+  } else {
+
+  let weather = JSON.parse(body);
+    let msg = `It's ${weather.results['sunrise']} for the sunrise at your location and  ${weather.results['sunset']} for the sunset in your location`
+    console.log(msg)
+  }
+  res.send(body)
+});
+
+})
 
 // simple post request
 app.post('/', function(req, res) {
